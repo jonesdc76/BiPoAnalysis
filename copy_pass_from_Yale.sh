871d94825d3bdf6cc3583731e566fe161c4b6295
jonesdc76 <jonesdc76@gmail.com>
2018-05-29 15:46:20 -0400

    Add files for checking runlist

--- copy_pass_from_Yale.sh
+++ copy_pass_from_Yale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pass=pass$1
-release=Phys_20180510
+release=Phys_20180518
 dir=prospect/data/Analyzed
 if [ $# -ne 1 ]
 then 
@@ -12,14 +12,20 @@ else
     series[${n}]=180417_Background; echo "Adding  ${series[$n]}"; ((n++))
     series[${n}]=180420_Background; echo "Adding  ${series[$n]}"; ((n++))
     series[${n}]=180501_ReactorOn; echo "Adding  ${series[$n]}";  ((n++))
+    
+    if [ ! -d ${BIPO_OUTDIR}/${release} ];then
+	mkdir ${BIPO_OUTDIR}/${release}
+    fi
 
     for i in "${series[@]}"
     do
 	rm -rf ${BIPO_OUTDIR}/${release}/${i}/${pass}/*.root
+	if [ ! -d ${BIPO_OUTDIR}/${release}/$i ];then
+	    mkdir ${BIPO_OUTDIR}/${release}/$i
+	fi
 	if [ ! -d ${BIPO_OUTDIR}/${release}/$i/${pass} ];then
 	    mkdir ${BIPO_OUTDIR}/${release}/$i/${pass}
 	fi
 	scp -r Yale:${dir}/${release}/${i}/${pass}/*.root ${BIPO_OUTDIR}/${release}/${i}/${pass}/
-    done
-
+    done;
 fi
