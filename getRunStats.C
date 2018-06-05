b38ab6ba30f1d304d4ffad77ebc3832fa6ef167c
jonesdc76 <jonesdc76@gmail.com>
2018-06-05 17:07:14 -0400

    Add getRunStats.C

--- getRunStats.C
+++ getRunStats.C
@@ -1,17 +1,24 @@
-{
+#include <iostream>
 #include <time.h>
+#include "BP.C"
+#include "TChainElement.h"
+#include "TString.h"
+#include "TFile.h"
+#include "TVectorD.h"
 
-  cout<<"Did you load/compile BP.C?"<<endl;
+int getRunStats(){  
   BP *bp = new BP();
   TChain *ch = bp->chain;
   TIter next(ch->GetListOfFiles());
   TChainElement *element;
   TString st, prev_st = "";
-  double tlive = 0, tl = 0;
+  double tlive = 0, tl = 0, prev_rt = 0;
   bool first = 1, first_of_dataset = 1;
   const int N=6;
   int n = 0;
   time_t ts1, ts2, prev_ts = 0;
+  TString month[12] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+  TString week[7] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
   TString data[N] = {"WetCommissioning", "180316_Background",  "180417_Background","180420_Background","180501_ReactorOn","180520_Background"};
   while((element = (TChainElement*)next())){
     st = TString(element->GetTitle());
@@ -19,12 +26,14 @@
       first_of_dataset = 0;
       ts1 = time_t(TString(st(st.First(".")-10,10)).Atoi());
     }
-    if(!st.Contains(data[n])){
+    if((!st.Contains(data[n])) && n < N-1){
       //ut<<st.Data()<<"\n"<<prev_st.Data()<<endl<<endl;
-      ts2 = prev_ts;
-      cout<<"\\item{"<<data[n].Data()<<": "<<Form("%0.3f",difftime(ts2, ts1)/3600.0)<<" hours of data from ";
-      printf("%s", asctime(localtime(&ts1)));
-      printf("to %s.}\n",asctime(localtime(&ts2)));
+      ts2 = prev_ts + tl;
+      cout<<"\\item{"<<data[n].ReplaceAll("_","\\_")<<": "<<Form("%0.3f",difftime(ts2, ts1)/3600.0)<<" hours of data from ";
+      auto t = localtime(&ts1);
+      printf("%s %s %i, %i %02i:%02i", week[t->tm_wday].Data(), month[t->tm_mon].Data(), t->tm_mday, t->tm_year+1900, t->tm_hour, t->tm_min);
+      t = localtime(&ts2);
+      printf(" to %s %s %i, %i %02i:%02i}\n", week[t->tm_wday].Data(), month[t->tm_mon].Data(), t->tm_mday, t->tm_year+1900, t->tm_hour, t->tm_min);
       ts1 = time_t(TString(st(st.First(".")-10,10)).Atoi());
       ++n;
     }
@@ -34,12 +43,19 @@
     if(first){
       first = 0;
       time_t ts = time_t(TString(st(st.First(".")-10,10)).Atoi());
-      printf("Date of first file: %s", asctime(localtime(&ts)));
-      printf("Date of first file: %s", asctime(localtime(&ts)));
+      printf("Start time of first file: %s", asctime(localtime(&ts)));
     }
     prev_st = st;
     prev_ts = time_t(TString(st(st.First(".")-10,10)).Atoi());
   }
   time_t ts = time_t(TString(st(st.First(".")-10,10)).Atoi() + tl);
-  printf("Date of last file: %s", asctime(localtime(&ts)));
+  cout<<"\\item{"<<data[n].ReplaceAll("_","\\_")<<": "<<Form("%0.3f",difftime(ts, ts1)/3600.0)<<" hours of data from ";
+  auto t = localtime(&ts1);
+  printf("%s %s %i, %i %02i:%02i", week[t->tm_wday].Data(), month[t->tm_mon].Data(), t->tm_mday, t->tm_year+1900, t->tm_hour, t->tm_min);
+  t = localtime(&ts);
+  printf(" to %s %s %i, %i %02i:%02i}\n", week[t->tm_wday].Data(), month[t->tm_mon].Data(), t->tm_mday, t->tm_year+1900, t->tm_hour, t->tm_min);
+  printf("End time of last file: %s", asctime(localtime(&ts)));
+  cout<<"Total hours of data: "<<tlive/3600.0<<"\n";
+  
+  return 0;
 }
