90b2551d8fadeff4d80a794bd09751e504aea57a
jonesdc76 <jonesdc76@gmail.com>
2018-07-09 15:27:22 -0400

    Added flexibility to copy_good_runs_v2.sh to allow for copying either IBD trees or BiPo.

--- copyTree.C
+++ copyTree.C
@@ -1,11 +1,11 @@
 #include "TFile.h"
 #include "TTree.h"
 
-void copyTree(const char *infname, const char *outfname) {
+void copyTree(const char *infname, const char *outfname, const char* treename= "BiPoTreePlugin/BiPo") {
 
    //Get old file, old tree and set top branch address
    TFile *oldfile = new TFile(infname);
-   TTree *oldtree = (TTree*)oldfile->Get("BiPoTreePlugin/BiPo");
+   TTree *oldtree = (TTree*)oldfile->Get(treename);
 
    //Create a new file + a clone of old tree in new file
    TFile *newfile = new TFile(outfname,"recreate");
--- copy_good_runs_v2.sh
+++ copy_good_runs_v2.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+IBD=0
+TREE=BiPoTreePlugin/BiPo
+FILE=AD1_Extra_Phys.root
+if [ $IBD -eq 1 ]; then
+    TREE=P2kIBDPlugin/Tibd
+    FILE=AD1_Wet_Phys.root
+fi
 RELEASE=Phys_NuFact_v1
 ANARELEASE=Analyzed_NuFact_v1
-FILE=AD1_Extra_Phys.root
+
 
 for i in $(cat NuFactGoodRuns.txt);do
     if [ -d ${P2X_ANADAT}/${ANARELEASE}/${i} ];then
 	dir="$(cut -d'/' -f1 <<<$i)"
 	    #echo $dir
-	if [ ! -d ${BIPO_OUTDIR}/${RELEASE}/${i} ];then
-	    mkdir -p ${BIPO_OUTDIR}/${RELEASE}/${i}
+	if [ ! -d ${BIPO_OUTDIR}/${ANARELEASE}/${i} ];then
+	    mkdir -p ${BIPO_OUTDIR}/${ANARELEASE}/${i}
 	fi
 	infname=${P2X_ANADAT}/${ANARELEASE}/${i}/$FILE
 	outfname=${BIPO_OUTDIR}/${ANARELEASE}/${i}/$FILE
 	if [ ! -f $outfname ];then
-	    root -b -l -q "copyTree.C+(\"$infname\", \"$outfname\")"
+	    root -b -l -q "copyTree.C+(\"$infname\", \"$outfname\", \"$TREE\")"
 	else
 	    echo "$outfname already exists. Skipping."
 	fi
