bcaf330a4839173cbdb5923aedf8efb4c246da26
jonesdc76 <jonesdc76@gmail.com>
2018-07-10 09:54:22 -0400

    Small changes

--- copy_IBDTree_from_Yale.sh
+++ copy_IBDTree_from_Yale.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 DATA_DIR=/projects/prospect/converted_data/Analyzed/
-RELEASE=Phys_Neutrino_v2
+DATA_DIR=/home/jonesdc/prospect/data/Analyzed/
+RELEASE=Phys_NuFact_v1
 ANARELEASE=Analyzed_NuFact_v1
 FILE=AD1_Wet_Phys.root
 SSHSOCKET=~/.ssh/jonesdc@wright.physics.yale.edu
 ssh -M -f -N -o ControlPath=$SSHSOCKET jonesdc@wright.physics.yale.edu
-for i in $(cat RunList.txt);do
+for i in $(cat NuFactGoodRuns.txt);do
     dir=$BIPO_OUTDIR/$ANARELEASE/$i
     if [ ! -d $dir ];then
 	mkdir -p $dir
