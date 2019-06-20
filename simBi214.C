8c62b044de04ed160413a3b3b310ab955af36951
jonesdc76 <jonesdc76@gmail.com>
2019-06-20 11:39:49 -0400

    Minor edits to scripts

---
+++ simBi214.C
@@ -0,0 +1,27 @@
+#include "TChain.h"
+#include "TH1D.h"
+#include "TFile.h"
+#include "BPsim.C"
+
+int simPo214(){
+  BPsim *bps = new BPsim();
+  TChain *ch = bps->chain;
+
+  ch->Draw("pEtot>>h(100,0,4)");
+  TH1D *h = (TH1D*)gDirectory->Get("h");
+  h->SetName("hSimBetaBi214");
+  TFile f("SimBi214BetaSpectrum.root","recreate");
+  h->GetXaxis()->SetTitle("AD Full Detector Po214 #beta Energy Spectrum");
+  h->GetXaxis()->SetTitle("#beta Energy (MeV)");
+  h->GetYaxis()->SetTitle("Counts (a.u.)");
+  h->Write();
+  ch->Draw("pE>>h76(100,0,4)","pseg==76");
+  TH1D *h76 = (TH1D*)gDirectory->Get("h76");
+  h76->GetXaxis()->SetTitle("Segment 76 Single Cell Po214 #beta Energy Spectrum");
+  h76->GetXaxis()->SetTitle("#beta Energy (MeV)");
+  h76->GetYaxis()->SetTitle("Counts (a.u.)");
+  h76->SetName("hCell76SimBetaBi214");
+  h76->Write();
+  return 0;
+  //f.Close();
+}
